aeccab1f84c8edd5e96b442fa49491c16e6870c6
Drainking <heavyyrain3039@gmail.com>
2021-04-16 01:52:55 +0300

    http зпросы на firebase/ добавление/ удаление task

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 import { DateService } from '../shared/date.service';
+import { Task, TaskService } from '../shared/task.service';
 
 @Component({
   selector: 'app-organizer',
@@ -10,19 +12,44 @@ import { DateService } from '../shared/date.service';
 export class OrganizerComponent implements OnInit {
 
   form: FormGroup
+  tasks: Task[] = []
 
   constructor(
-    public dateService: DateService
+    public dateService: DateService,
+    private tasksService: TaskService
   ) { }
 
   ngOnInit(): void {
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
     this.form = new FormGroup({
       title: new FormControl('', Validators.required)
     })
   }
 
   submit() {
-    
+    const {title} = this.form.value
+
+    const task: Task = {
+      title,
+      date: this.dateService.date.value.format('DD-MM-YYYY')
+    }
+
+    this.tasksService.create(task).subscribe((task) => {
+      this.tasks.push(task)
+      this.form.reset()
+    }, err => console.error(err))
+
+  }
+
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter( t => t.id !== task.id)
+    }, err => console.error(err))
   }
 
 }
--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,7 +1,48 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import * as moment from "moment";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+export interface Task {
+    id?: string
+    title: string
+    date?: string
+}
+
+interface CreateResponse {
+    name: string
+}
 
 @Injectable({providedIn: 'root'})
 
 export class TaskService {
     static url = 'https://calendar-39add-default-rtdb.firebaseio.com/task'
+
+    constructor(private http: HttpClient) {
+        
+    }
+
+    load(date: moment.Moment): Observable<Task[]> {
+        return this.http.get<Task[]>(`${TaskService.url}/${date.format('DD-MM-YYYY')}.json`)
+        .pipe(map((tasks) => {
+            if(!tasks) {
+                return []
+            } else {
+                return Object.keys(tasks).map(key => ({...tasks[key], id: key}))
+            }
+        }))
+    }
+
+    create(task: Task): Observable<Task> {
+        return this.http.post<CreateResponse>(`${TaskService.url}/${task.date}.json`, task)
+        .pipe(map((res) => {
+            return {...task, id: res.name}
+        }))
+    }
+
+    remove(task: Task): Observable<void> {
+        return this.http.delete<void>(`${TaskService.url}/${task.date}/${task.id}.json`)
+    }
+
 }
